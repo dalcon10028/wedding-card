c6b89fe3163a8df44fcd5d36550fb16e0e0b006c
corn <dalcon10280@gmail.com>
2024-11-24 13:42:05 +0900

    chore: config tailwind and primevue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ['@nuxtjs/tailwindcss', '@primevue/nuxt-module'],
+  css: ['@/assets/styles/tailwind.css', '@/assets/styles/base.css'],
+  primevue: {
+    options: { theme: 'none' }
+  },
+  postcss: {
+    plugins: {
+      'postcss-import': {},
+      tailwindcss: {},
+      autoprefixer: {}
+    }
+  }
 })
